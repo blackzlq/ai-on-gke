83176acf5b1e399f97f2e86549325a5c250515a2
Liqin Zhang😈 <zlq@google.com>
2024-04-11 18:45:51 +0000

    IAP test
    ROLL BACK THE IMAGE
    Tested-by: zlq

--- modules/iap/tests/iap_test.py
+++ modules/iap/tests/iap_test.py
@@ -1,4 +1,5 @@
-import sys
+import argparse
+import json
 import os
 import requests
 from google.auth.transport.requests import Request
@@ -6,7 +7,6 @@ from google.oauth2 import id_token
 from googleapiclient import discovery
 import google.auth
 from google.cloud import resourcemanager_v3
-
 
 def get_project_number(project_id):
   # Create a Resource Manager client
@@ -24,29 +24,15 @@ def get_project_number(project_id):
   print(response)
   return response.name.split('/')[1]
 
-
 def get_project_id():
   id = ""
   if 'GCP_PROJECT' in os.environ:
     id = os.environ['GCP_PROJECT']
-
-  # else if this is running locally then GOOGLE_APPLICATION_CREDENTIALS should be defined
   elif 'GOOGLE_APPLICATION_CREDENTIALS' in os.environ:
     with open(os.environ['GOOGLE_APPLICATION_CREDENTIALS'], 'r') as fp:
       credentials = json.load(fp)
     id = credentials['project_id']
   return id
-
-frontend_url = sys.argv[1]
-frontend_client_id = sys.argv[2]
-jupyter_url = sys.argv[3]
-jupyter_client_id = sys.argv[4]
-ray_dashboard_url = sys.argv[5]
-ray_dashboard_client_id = sys.argv[6]
-project_id = get_project_id()
-namespace = sys.argv[8]
-
-print(f'get sys argv 7 project id {sys.argv[7]}')
 
 def list_backend_services_ids(project_id, keyword):
   credentials, _ = google.auth.default()
@@ -62,17 +48,15 @@ def list_backend_services_ids(project_id, keyword):
 
   return filtered_service_ids
 
-
 def make_iap_request(url, client_id, keyword, method="GET", **kwargs):
   if "timeout" not in kwargs:
     kwargs["timeout"] = 90
 
-  # List GCP backend services IDs based on the project ID and keyword
+  # project_id = get_project_id()
   gcp_backend_services_ids = list_backend_services_ids(project_id, f'{namespace}-{keyword}')
   print("GCP Backend Services IDs:", gcp_backend_services_ids)
   project_number = get_project_number(project_id)
 
-  # Construct expected audiences from the GCP backend services IDs
   expected_audiences = [f"/projects/{project_number}/global/backendServices/{service_id}" for service_id in gcp_backend_services_ids]
   print("Expected Audiences:", expected_audiences)
 
@@ -86,38 +70,45 @@ def make_iap_request(url, client_id, keyword, method="GET", **kwargs):
         headers={"Authorization": "Bearer {}".format(open_id_connect_token)},
         **kwargs
     )
-    # If the request was successful, you can now process the response
     print(resp.text)
     if resp.status_code == 403:
-      raise Exception(
-          "Service account does not have permission to "
-          "access the IAP-protected application."
-      )
+      raise Exception("Service account does not have permission to access the IAP-protected application.")
     elif resp.status_code != 200:
-      raise Exception(
-          "Bad response from application: {!r} / {!r} / {!r}".format(
-              resp.status_code, resp.headers, resp.text
-          )
-      )
+      raise Exception("Bad response from application: {!r} / {!r} / {!r}".format(resp.status_code, resp.headers, resp.text))
     else:
       return resp.text
   except requests.exceptions.RequestException as e:
-    # This will catch any errors during the request
     print(e)
 
+# Define the argument parser and add arguments
+parser = argparse.ArgumentParser(description="Script to test GCP backend services via IAP.")
+parser.add_argument('frontend_url', help="URL of the frontend service")
+parser.add_argument('frontend_client_id', help="Client ID for the frontend service")
+parser.add_argument('jupyter_url', help="URL of the Jupyter service")
+parser.add_argument('jupyter_client_id', help="Client ID for the Jupyter service")
+parser.add_argument('ray_dashboard_url', help="URL of the Ray dashboard")
+parser.add_argument('ray_dashboard_client_id', help="Client ID for the Ray dashboard")
+parser.add_argument('project_id', help="GCP Project ID")
+parser.add_argument('namespace', help="Namespace for the backend services")
 
+# Parse the arguments
+args = parser.parse_args()
+
+# Use the parsed arguments in the rest of your script
+project_id = args.project_id
+namespace = args.namespace
 
 def test_jupyter():
-  r = make_iap_request(jupyter_url, jupyter_client_id, "jupyter")
-  print(r.content.decode('utf-8'))
+  r = make_iap_request(args.jupyter_url, args.jupyter_client_id, "jupyter")
+  print(r)
 
 def test_frontend():
-  r = make_iap_request(frontend_url, frontend_client_id, "frontend")
-  print(r.content.decode('utf-8'))
+  r = make_iap_request(args.frontend_url, args.frontend_client_id, "frontend")
+  print(r)
 
 def test_ray_dashboard():
-  r = make_iap_request(ray_dashboard_url, ray_dashboard_client_id, "ray-dashboard")
-  print(r.content.decode('utf-8'))
+  r = make_iap_request(args.ray_dashboard_url, args.ray_dashboard_client_id, "ray-dashboard")
+  print(r)
 
 test_jupyter()
 test_frontend()
